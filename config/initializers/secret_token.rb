6f41583f20a6370143f455e4a414e388729a2a6a
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2013-10-21 19:42:45 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calisto::Application.config.secret_key_base = '7b9b8043fdb61383e8e8a97d612750ce00894c0003a36b02837660295060d962d9d86c0ca60db984d73eed7488e1ad7ff4645d670dc86386d8fa1f26f1c2dc23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Calisto::Application.config.session_store :cookie_store, key: '_calisto_session'
